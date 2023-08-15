27860d2f36259c9e0f7cb9218402b9ba1a9d948f
huiryong <kou16955@gmail.com>
2023-08-15 23:03:28 +0900

    second

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,57 @@
 import React from "react";
+import { styled } from "styled-components";
+
+const Wrapper = styled.section`
+  background-color: pink;
+  height: 100vh;
+`;
+
+const AboutTitle = styled.h2`
+  font-size: 50px;
+  text-align: center;
+  padding: 50px 0;
+  border-bottom: 1px solid black;
+`;
+
+const Name = styled.h3`
+  font-size: 60px;
+  > p {
+    font-size: 16px;
+  }
+`;
+
+const Skill = styled.h3`
+  font-size: 30px;
+`;
+
+const SkillContent = styled.div``;
+
+const Plofile = styled.h2``;
 
 const About = () => {
-  return <div>about</div>;
+  return (
+    <Wrapper>
+      <AboutTitle>
+        <h2>About</h2>
+      </AboutTitle>
+      <Name>
+        <h3>高希怜(Kou HuiRyong)</h3>
+        <p>
+          7年間幼稚園に勤務していましたが、子どもを抱っこし過ぎて腰をいわしました…そこで別業種に転職を決意、職業訓練を通してwebデザインやフロントに興味を持ちました。{" "}
+        </p>
+      </Name>
+
+      <Skill>
+        <h3>スキル</h3>
+        <SkillContent>
+          <p>言語：HTML/CSS/JavaScript/jQuery/React</p>
+          <p>ツール：Illustrator/Photoshop</p>
+          <p>その他：Word/PowerPoint</p>
+        </SkillContent>
+      </Skill>
+      <Plofile>経歴</Plofile>
+    </Wrapper>
+  );
 };
 
 export default About;
--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,7 +1,71 @@
 import React from "react";
+import { styled } from "styled-components";
+
+const Wrapper = styled.section`
+  background-color: pink;
+  height: 100vh;
+  > h2 {
+    text-align: center;
+    border-bottom: 1px solid black;
+  }
+`;
+const WorkContent = styled.div`
+  display: flex;
+  padding: 60px;
+  > h2 {
+    text-align: center;
+    border-bottom: 1px solid black;
+  }
+`;
+const Text = styled.div`
+  width: 40%;
+  > h3 {
+    font-size: 60px;
+    margin-bottom: 30px;
+  }
+`;
+
+const ImgBlock = styled.div`
+  width: 60%;
+  > img {
+    width: 100%;
+    height: 80%;
+    object-fit: cover;
+  }
+`;
+
+const Website = styled.div``;
+const App = styled.div``;
+const Practice = styled.div``;
 
 const Work = () => {
-  return <div>Work</div>;
+  return (
+    <Wrapper>
+      <h2>Work</h2>
+      <WorkContent>
+        <Text>
+          <Website>
+            <h3>website</h3>
+            <p>仮想サイトの制作</p>
+            <a href="#">01</a>
+            <a href="#">02</a>
+          </Website>
+          <App>
+            <h3>app</h3>
+            <p>Reactを用いたアプリの制作</p>
+            <a href="#">01</a>
+          </App>
+          <Practice>
+            <h3>practice</h3>
+            <a href="#">その他、模写コーディングなど</a>
+          </Practice>
+        </Text>
+        <ImgBlock>
+          <img src="images/flower.jpg" alt="" />
+        </ImgBlock>
+      </WorkContent>
+    </Wrapper>
+  );
 };
 
 export default Work;
